48d3e1c32ad1332cafec4eaa95ca1290677457ca
Aredu89 <aredu_r89@hotmail.com>
2025-04-28 10:49:49 -0500

    Add AG-Grid and set a basic working example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { ClientSideRowModelModule } from "ag-grid-community";
+import { ModuleRegistry, themeBalham } from "ag-grid-community";
 
-function App() {
+// Register modules before using them
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const App = () => {
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make", filter: true, editable: true },
+    { field: "model", filter: true, editable: true },
+    { field: "price", filter: true, editable: true },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600, margin: "auto", marginTop: "50px" }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        pagination={true}
+        paginationPageSize={2}
+        theme={themeBalham}
+      />
     </div>
   );
-}
+};
 
 export default App;
